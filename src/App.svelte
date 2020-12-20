5bdde5a5476bba59d92420f6d145a98ca0303795
Hassan Bouzlima <bouzlima.h@live.fr>
2020-12-20 14:26:22 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Nested from "./nested.svelte";
+	export let name;
+	let msg = "";
+	function handleMessage(event) {
+		msg = event.detail.text;
+	}
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+	{msg}
+	<Nested on:message={handleMessage} />
+</main>
---
+++ src/nested.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function random() {
+        setTimeout(() => {
+            let x = Math.random();
+            dispatch("message", { text: x });
+        }, 2000);
+    }
+</script>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
+
+<button on:click={random}> Get random number </button>
